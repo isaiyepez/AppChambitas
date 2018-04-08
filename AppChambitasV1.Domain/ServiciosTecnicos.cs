27af6b442f7bca35ac4197f7961d53ee1bf86ad7
iyepez <isaiyepez@gmail.com>
2018-04-07 19:13:16 -0500

    ServiciosTecnicos
    Agregado de ServiciosTecnicos

---
+++ AppChambitasV1.Domain/ServiciosTecnicos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace AppChambitasV1.Domain
+{
+    public class ServiciosTecnicos
+    {
+        [Key]
+        [Display(Name = "ServiciosTecnicos_ID")]
+        public int ServTecn_ID { get; set; }      
+
+        [JsonIgnore]
+        public virtual ICollection<Tecnico> Tecnico { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<TiposServiciosDetalle> TiposServiciosDetalle { get; set; }
+
+        [Display(Name = "Activo")]
+        public bool ServTecn_Activo { get; set; }
+    }
+}
